8c26526d7d256cc7bc1f949b973469cda54edb61
Tessyrich <olyrich3@gmail.com>
2022-10-17 21:01:31 -0700

    fixed errors

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const hamburger = document.querySelector('.hamburger')
-const menuBar = document.querySelector('.menu-bar')
+const hamburger = document.querySelector('.hamburger');
+const menuBar = document.querySelector('.menu-bar');
 
-hamburger.addEventListener('click', () =>{
-  
-  hamburger.classList.toggle('active')
-    menuBar.classList.toggle('show-navbar')
- 
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menuBar.classList.toggle('show-navbar');
+});
+document.querySelectorAll('.head-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menuBar.classList.remove('show-navbar');
+}));
