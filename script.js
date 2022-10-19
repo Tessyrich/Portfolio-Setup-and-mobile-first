4f6988840479950461bdc38de4439d2ec16fd490
Tessyrich <olyrich3@gmail.com>
2022-10-19 15:39:52 -0700

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
-const menuBar = document.querySelector('.menu-bar');
+const menuBar = document.querySelector('.menu-bar'); 
+const popUp = document.getElementById('see');
+const popContainer = document.getElementById('pop-container');
+const closePop = document.getElementById('close-pop')
+
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -9,3 +13,15 @@ document.querySelectorAll('.head-link').forEach((n) => n.addEventListener('click
   hamburger.classList.remove('active');
   menuBar.classList.remove('show-navbar');
 }));
+
+
+popUp.addEventListener('click', OpenPopup);
+closePop.addEventListener('click', ClosePopUp)
+
+
+function OpenPopup(){
+  popContainer.style.display = 'block'
+}
+function ClosePopUp(){
+  popContainer.style.display = 'none'
+}
